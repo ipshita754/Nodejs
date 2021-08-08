33ed3470852854a21edcf31f248b0076f03ba10d
Ipshita <ipshita754@gmail.com>
2021-08-08 23:46:59 +0530

    nodejs Program

---
+++ eg7.js
@@ -0,0 +1,23 @@
+var x;
+x=1;
+while(x<=5)
+{
+console.log(x);
+x++;
+}
+
+using For loop
+
+var x;
+for(x=1;x<=5;x++)
+console.log(x);
+
+
+Using do While loop
+
+var x;
+x=1;
+do{
+console.log(x);
+x++;
+}while(x<=5);
